0e63b301e256a40b84002f15fef8c2f5216d51dd
Dark-Corp <53129044+dark-corp@users.noreply.github.com>
2020-11-14 03:24:37 -0300

    Add files via upload

---
+++ css/menu.js
@@ -0,0 +1,9 @@
+
+/* Setar largura 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+/* Setar largura  0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
